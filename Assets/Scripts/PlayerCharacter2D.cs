701188e2f0e4b1c8238fb072568a70f2c985d067
Rodger Scott <rodger.scott@gmail.com>
2018-06-04 17:51:29 -0500

    Basic x axis movement and idle animations added.

---
+++ Assets/Scripts/PlayerCharacter2D.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter2D : MonoBehaviour {
+
+    public int speed;
+    public SpriteRenderer sr;
+    public Animator anim;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Movement();
+	}
+
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            anim.SetBool("isWalking", true);
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+            sr.flipX = true;
+
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            anim.SetBool("isWalking", true);
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+            sr.flipX = false;
+        }
+        else
+            anim.SetBool("isWalking", false);
+    }
+}
