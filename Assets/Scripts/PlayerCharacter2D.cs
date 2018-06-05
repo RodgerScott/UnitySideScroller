581b07bb1852b8b50678a0f11f565542d8a0ea4c
Rodger Scott <rodger.scott@gmail.com>
2018-06-05 14:29:31 -0500

    Added shooting animationa nd jump gravity

--- Assets/Scripts/PlayerCharacter2D.cs
+++ Assets/Scripts/PlayerCharacter2D.cs
@@ -10,29 +10,58 @@ public class PlayerCharacter2D : MonoBehaviour {
     public float jumpForce;
     public Rigidbody2D rig;
     public bool isGrounded;
+    bool shotTimer;
+    float shootTimer = .2f;
 	// Use this for initialization
 	void Start () {
         isGrounded = false;
+        shotTimer = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if (shotTimer)
+        shootTimer -= Time.deltaTime;
+        if(shootTimer <= 0)
+        {
+            shotTimer = false;
+            anim.SetBool("isShooting", false);
+            shootTimer = .2f;
+        }
         Movement();
+        if (isGrounded) anim.SetBool("isGrounded", true);
+        else anim.SetBool("isGrounded", false);
+        if(rig.velocity.y < 0)
+        {
+            rig.velocity += Vector2.up * Physics2D.gravity.y * 2 * Time.deltaTime;
+        }
 	}
 
     void Movement()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!shotTimer)
+            {
+                anim.SetBool("isShooting", true);
+                shotTimer = true;
+            }
+            
+
+        }
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (isGrounded)
             {
                 rig.AddForce(Vector2.up * jumpForce);
+                anim.SetBool("isShooting", false);
                 isGrounded = false;
             }
         }
         if (Input.GetKey(KeyCode.D))
         {
             anim.SetBool("isWalking", true);
+            anim.SetBool("isShooting", false);
             transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
             sr.flipX = true;
 
@@ -40,6 +69,7 @@ public class PlayerCharacter2D : MonoBehaviour {
         else if (Input.GetKey(KeyCode.A))
         {
             anim.SetBool("isWalking", true);
+            anim.SetBool("isShooting", false);
             transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
             sr.flipX = false;
         }
@@ -52,6 +82,7 @@ public class PlayerCharacter2D : MonoBehaviour {
         if(col.gameObject.tag == "Ground")
         {
             isGrounded = true;
+            anim.SetBool("isGrounded", true);
         }
     }
 }
