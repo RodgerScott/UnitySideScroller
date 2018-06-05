bc44e6ce7633baa1e1203798bdcdcc50fc73d95b
Rodger Scott <rodger.scott@gmail.com>
2018-06-05 13:28:50 -0500

    Implemented basic jumping

--- Assets/Scripts/PlayerCharacter2D.cs
+++ Assets/Scripts/PlayerCharacter2D.cs
@@ -7,9 +7,12 @@ public class PlayerCharacter2D : MonoBehaviour {
     public int speed;
     public SpriteRenderer sr;
     public Animator anim;
+    public float jumpForce;
+    public Rigidbody2D rig;
+    public bool isGrounded;
 	// Use this for initialization
 	void Start () {
-		
+        isGrounded = false;
 	}
 	
 	// Update is called once per frame
@@ -19,6 +22,14 @@ public class PlayerCharacter2D : MonoBehaviour {
 
     void Movement()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded)
+            {
+                rig.AddForce(Vector2.up * jumpForce);
+                isGrounded = false;
+            }
+        }
         if (Input.GetKey(KeyCode.D))
         {
             anim.SetBool("isWalking", true);
@@ -34,5 +45,13 @@ public class PlayerCharacter2D : MonoBehaviour {
         }
         else
             anim.SetBool("isWalking", false);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
     }
 }
